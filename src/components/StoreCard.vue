1182422d5a2467ef4f257f0fa676815abd485449
ErikWijnbladh <wijnbladherik@gmail.com>
2025-03-06 11:32:41 +0100

    Update StoreCard.vue

--- src/components/StoreCard.vue
+++ src/components/StoreCard.vue
@@ -85,13 +85,12 @@ const directionsUrl = computed(() => {
       class="block mt-3 text-blue-600 hover:text-blue-800 hover:underline"
     >
       <div class="flex items-start">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 mt-0.5 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 my-auto flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
         </svg>
         <div>
-          <p>{{ storeItem.content?.Address }}</p>
-          <p>{{ storeItem.content?.City }}, {{ storeItem.content?.State }} {{ storeItem.content?.Zip }}</p>
+          <p>{{ storeItem.content?.Address }}, {{ storeItem.content?.City }}, {{ storeItem.content?.Zip }}</p>
         </div>
       </div>
     </a>
